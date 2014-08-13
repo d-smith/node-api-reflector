4b909d8a74f7355d96afe08ab8fd470f7ed423a0
d-smith <doug.smith.mail@gmail.com>
2014-08-13 16:17:16 -0600

    Mods to inject service implementation used by handler

--- app/handlers/accessTokenHandlers.js
+++ app/handlers/accessTokenHandlers.js
@@ -2,23 +2,27 @@ var _ = require('underscore')
 var qs = require('querystring');
 var accessToken = require('../services/accessToken.js')
 
-module.exports = new function() {
-  this.createAccessToken = function(request, response) {
-    var data = '';
-    request.on('data', function(chunk) {
-      data += chunk;
-    });
 
-    request.on('end', function() {
-      var post = qs.parse(data);
-      console.log(post);
-      var atoken = accessToken.getAccessToken(post.username, post.password);
-      if(_.has(atoken, "error")) response.status(401)
-      response.send(atoken)
-    });
-  };
+module.exports = function(accessTokenSvc) {
+  return {
+    createAccessToken: function(request, response) {
+      var data = '';
+      request.on('data', function(chunk) {
+        data += chunk;
+      });
 
-  this.revokeAccessToken = function(request,response) {
-    response.status(200).send("");
+      request.on('end', function() {
+        var post = qs.parse(data);
+        console.log(post);
+        var atoken = accessToken.getAccessToken(post.username, post.password);
+        if(_.has(atoken, "error")) response.status(401)
+        response.send(atoken)
+      });
+    },
+
+    revokeAccessToken : function(request, response) {
+      response.status(200).send("");
+    }
+
   };
-};
+}
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,8 @@
-var accessTokenHandlers = require('./handlers/accessTokenHandlers')
+var accessTokenService = require('./services/accessToken');
+var accessTokenHandlers = require('./handlers/accessTokenHandlers');
+var serviceAccessTokenHandlers = accessTokenHandlers(accessTokenService);
 
 module.exports = function(app) {
-  app.post('/v1/xtrac/oauth2/token', accessTokenHandlers.createAccessToken);
-  app.post('/v1/xtrac/oauth2/revoke', accessTokenHandlers.revokeAccessToken); 
+  app.post('/v1/xtrac/oauth2/token', serviceAccessTokenHandlers.createAccessToken);
+  app.post('/v1/xtrac/oauth2/revoke', serviceAccessTokenHandlers.revokeAccessToken); 
 }

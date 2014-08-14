3dd0f828adafb1d7ef339807e38c7cb48f91ca0c
d-smith <doug.smith.mail@gmail.com>
2014-08-14 13:39:17 -0600

    mods to allow injection of services used in request handling

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,9 @@
 var accessTokenService = require('./services/accessToken');
 var accessTokenHandlers = require('./handlers/accessTokenHandlers');
-var serviceAccessTokenHandlers = accessTokenHandlers(accessTokenService);
 
-module.exports = function(app) {
+
+module.exports = function(app, services) {
+  var serviceAccessTokenHandlers = accessTokenHandlers(services["accessTokenService"]);
   app.post('/v1/xtrac/oauth2/token', serviceAccessTokenHandlers.createAccessToken);
-  app.post('/v1/xtrac/oauth2/revoke', serviceAccessTokenHandlers.revokeAccessToken); 
+  app.post('/v1/xtrac/oauth2/revoke', serviceAccessTokenHandlers.revokeAccessToken);
 }
--- server.js
+++ server.js
@@ -3,8 +3,11 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 
+var services = {};
+services["accessTokenService"] = require('./app/services/accessToken');
+
 //load routes
-require('./app/routes')(app);
+require('./app/routes')(app,services);
 
 //Start the server listener
 var port = process.env.PORT || 8666;

1e94fb2c0fe2188f86366c6832b0dd0e1e6eec8b
d-smith <doug.smith.mail@gmail.com>
2014-08-21 13:39:06 -0600

    Added basic search functionality

--- app/handlers/searchHandler.js
+++ app/handlers/searchHandler.js
@@ -1,9 +1,59 @@
+if (typeof String.prototype.startsWith != 'function') {
+  String.prototype.startsWith = function (str){
+    return this.slice(0, str.length) == str;
+  };
+}
+
+if (typeof String.prototype.endsWith != 'function') {
+  String.prototype.endsWith = function (str){
+    return this.slice(-str.length) == str;
+  };
+}
+
+
+var gobbleRemainingValueTokens = function(startingVal, tokens, startWith) {
+  if(startingVal.endsWith("'")) {
+    return startingVal.slice(1,-1);
+  }
+
+  var value = startingVal;
+  for(var idx = startWith; idx < tokens.length; idx++) {
+    var token = tokens[idx];
+    console.log('process ' + tokens[idx]);
+    value += " ";
+    value += tokens[idx];
+    if(token.endsWith("'")) {
+      value = value.slice(1,-1);
+      break;
+    }
+  }
+  return value;
+}
+
+var memoParser = function(filterCriteria) {
+  for(idx in filterCriteria) {
+    var criteria = filterCriteria[idx];
+    var tokens = criteria.split(" ");
+    var fieldName = (tokens[0].split(":"))[1];
+    if(fieldName == "Memo") {
+      var value = (tokens[2].split(":"))[1];
+      if(value.startsWith("'")) {
+        value = gobbleRemainingValueTokens(value, tokens, 3);
+      }
+      return value;
+    }
+  }
+
+  return null;
+}
+
+
 module.exports = function(searchSvc) {
   return {
     findTasks: function(request, response) {
       console.log("findTasks called with query " + request.query);
-      console.log(request.query.filterCriteria);
-      response.send(searchSvc.findTasks("foo"));
+      var memoSerchVal = memoParser(request.query.filterCriteria);
+      response.send(searchSvc.findTasks(memoSerchVal));
     }
   };
 }
--- app/services/search.js
+++ app/services/search.js
@@ -1,5 +1,25 @@
+var _ = require('underscore');
+
 module.exports = new function() {
+  var sampleItems = [
+  {"workItemNo":"W000001-08AUG14", "memo":"Ready for approval"},
+  {"workItemNo":"W000002-08AUG14", "memo":"Ready for approval"},
+  {"workItemNo":"W000003-08AUG14", "memo":"Not ready for approval"},
+  {"workItemNo":"W000004-08AUG14", "memo":"Ready for approval I guess"},
+  {"workItemNo":"W000005-08AUG14", "memo":"Ready for approval now"},
+  {"workItemNo":"W000006-08AUG14", "memo":"Could you approve this already"},
+  {"workItemNo":"W000007-08AUG14", "memo":"Ready for rejection"},
+  {"workItemNo":"W000008-08AUG14", "memo":"Reject-a-mundo"},
+  {"workItemNo":"W000009-08AUG14", "memo":"Approve?"},
+  {"workItemNo":"W000010-08AUG14", "memo":"Ready for approval"}
+  ];
+
+
   this.findTasks = function(memo) {
-    return [{'place':'holder'}];
+    console.log('findTasks called with arg ' + memo);
+    if(memo == undefined) {
+      return [];
+    }
+    return _.filter(sampleItems, function(theObj) { return theObj.memo == memo; });
   }
 };

18c928e2ee0862cc7306099ca03fdbd6963cd361
d-smith <doug.smith.mail@gmail.com>
2014-08-25 08:54:59 -0600

    Added unit tests for retrieve

--- tests/search.spec.js
+++ tests/search.spec.js
@@ -1,5 +1,7 @@
+var search = require("../app/services/search");
+
 describe('A search finds items that match work items', function()  {
-	var search = require("../app/services/search");
+	
 
 	it('Matches items by memo', function(done) {
 		var items = search.findTasks('Ready for approval');
@@ -11,6 +13,21 @@ describe('A search finds items that match work items', function()  {
 	it('Returns an empty array if there is no match', function(done) {
 		var items = search.findTasks('XXXXX No Match XXXXX');
 		expect(items.length).toBe(0);
+		done();
+	});
+});
+
+
+describe('Items can be retrieved directly', function(done) {
+	it('Returns an item if one for the item no exists', function(done) {
+		var item = search.findItem('W000005-08AUG14');
+		expect(item.workItemNo).toBe('W000005-08AUG14');
+		done();
+	});
+
+	it('Returns null if no march is found', function(done) {
+		var item = search.findItem('W000123-01APR95');
+		expect(item).toBe(null);
 		done();
 	});
 });
